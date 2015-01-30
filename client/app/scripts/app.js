26580064f957cea9cde36bd86e0a9246183d7c39
Greg Dziemidowicz <greg@cogent.co>
2015-01-30 21:09:52 +1100

    simple login form

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,20 +1,23 @@
 'use strict';
- 
+
 var app = angular.module('valpaldotrocksApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ng-token-auth'
   ]);
- 
+
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'views/main.html',
       controller: 'MainCtrl'
     })
+    .when('/sign_in', { templateUrl: 'views/user_sessions/new.html',
+      controller: 'UserSessionsCtrl' })
     .when('/about', {
       templateUrl: 'views/about.html',
       controller: 'AboutCtrl'
@@ -27,7 +30,7 @@ app.config(function ($routeProvider) {
       redirectTo: '/'
     });
 });
- 
+
 app.factory('Pal', ['$resource', function($resource) {
   return $resource('/api/pals/:id.json', null, {
     'update': { method:'PUT' }
---
+++ client/app/scripts/controllers/user_sessions.js
@@ -0,0 +1,4 @@
+'use strict';
+angular.module('valpaldotrocksApp')
+  .controller('UserSessionsCtrl', ['$scope', function ($scope) {
+  }]);
